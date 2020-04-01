1bdbba20f290abbd7f31ec9c173e0a7e49bbc41a
Edgar Eloy Munoz <edgareloymunoz@gmail.com>
2020-04-01 17:44:36 -0500

    Create NavBar Component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from '../Header/Header';
+import NavBar from '../NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <NavBar />
     </div>
   );
 }
---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <div className="NavBar">
+      <h2>NavBar</h2>
+    </div>
+  );
+}
+
+export default NavBar;
