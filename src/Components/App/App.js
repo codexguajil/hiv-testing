927ef8a32cbb5b06c1d015ab47ff37c16e527e49
Edgar Eloy Munoz <edgareloymunoz@gmail.com>
2020-04-01 17:43:37 -0500

    Create Header Component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from '../Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <Header />
     </div>
   );
 }
