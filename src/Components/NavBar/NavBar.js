700fb45ceeaea39cc4e486fd29ba7ea0c1b200e2
Edgar Eloy Munoz <edgareloymunoz@gmail.com>
2020-04-01 19:38:31 -0500

    Login Component and Nav

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, NavLink } from "react-router-dom";
 import About from '../About/About';
+import LogIn from '../../Containers/LogIn/LogIn';
 
 function NavBar() {
   return (
@@ -8,7 +9,15 @@ function NavBar() {
       <NavLink to="/About" className="nav">
         About
       </NavLink>
+      <NavLink to="/MakeAnAppointment" className="nav">
+        Make An Appointment
+      </NavLink>
+      <NavLink to="/LogIn" className="nav">
+        LogIn
+      </NavLink>
       <Route path="/About" component={About} />
+      <Route path="/MakeAnAppointment" />
+      <Route path="/LogIn" component={LogIn} />
     </div>
   );
 }
---
+++ src/Containers/LogIn/LogIn.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class LogIn extends Component {
+  render() {
+    return (
+      <div className="LogIn">
+        <h3>This is where you log in.</h3>
+        <input />
+        <input />
+      </div>
+    );
+  }
+}
+
+export default LogIn;
